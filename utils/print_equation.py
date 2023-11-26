9196dea0557d8a273af975b62d7dc7bf0c84af75
Your Name <you@example.com>
2023-11-26 09:32:54 +0545

    Formatting equation changes

--- utils/print_equation.py
+++ utils/print_equation.py
@@ -1,4 +1,8 @@
 # utils/print_equation.py
+import math
+
+def sign(num):
+    return '-' if num < 0 else '+'
 
 def print_equation(coefficients, constants):
     num_variables = len(constants)
@@ -7,7 +11,12 @@ def print_equation(coefficients, constants):
         equation = ""
         for j in range(num_variables):
             variable = chr(ord('a') + j)
-            equation += f"{coefficients[i][j]}{variable} + "
-        equation = equation[:-2]  # Remove the last '+'
+            coefficient = coefficients[i][j]
+
+            if coefficient != 0:
+                sign_str = sign(coefficient)
+                coefficient_str = f"{abs(coefficient)}" if abs(coefficient) != 1 else ""
+                equation += f" {sign_str} {coefficient_str}{variable}"
+
         print(f"{equation} = {constants[i]}")
-    print("\n")
+
